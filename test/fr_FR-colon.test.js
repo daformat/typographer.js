2955b98bd30678c4c14288015cf01e33e67e088e
daformat <mat.jouhet@gmail.com>
2019-03-23 16:12:09 +0100

    Add more tests

--- test/fr_FR-colon.test.js
+++ test/fr_FR-colon.test.js
@@ -5,3 +5,15 @@ test('Use non-breaking space before `:`', () => {
     typographer('Bonjour typographer: enchanté de faire votre connaissance.')
   ).toBe('Bonjour typographer\u00a0: enchanté de faire votre connaissance.');
 });
+
+test('Replace multiple spaces with a single non-breaking space before `:`', () => {
+  expect(
+    typographer('Bonjour typographer    : enchanté de faire votre connaissance.')
+  ).toBe('Bonjour typographer\u00a0: enchanté de faire votre connaissance.');
+});
+
+test('Replace any type of spaces with a single non-breaking space before `:`', () => {
+  expect(
+    typographer('Bonjour typographer \f\n\r\t\v\u00a0\u1680\u2000\u200a\u2028\u2029\u202f\u205f\u3000\ufeff: enchanté de faire votre connaissance.')
+  ).toBe('Bonjour typographer\u00a0: enchanté de faire votre connaissance.');
+});
