902407a019320ce263ab4dcc2556242213e4036e
daformat <mat.jouhet@gmail.com>
2019-03-23 21:26:28 +0100

    Add tests for parentheses and brackets

---
+++ test/fr_FR-parentheses.test.js
@@ -0,0 +1,11 @@
+import typographer from '../dist/index.js';
+
+test('[text] Remove wrapping spaces within ()', () => {
+  expect(
+    typographer({
+      locale: 'fr_FR',
+      output_format: 'text',
+      string: 'Bonjour (   ou bonsoir \f\n\r\t\v\u00a0\u1680\u2000\u200a\u2028\u2029\u202f\u205f\u3000\ufeff) typographer'
+    })
+  ).toBe('Bonjour (ou bonsoir) typographer');
+});
