d053569d19e66519ae1c62281088f86b2a72a5b4
daformat <mat.jouhet@gmail.com>
2019-03-23 20:00:46 +0100

    Update html test

--- test/fr_FR-html.test.js
+++ test/fr_FR-html.test.js
@@ -5,9 +5,9 @@ test('[text] Can format html strings', () => {
     typographer({
       locale: 'fr_FR',
       output_format: 'text',
-      string: '<p>Bonjour <b>typographer</b>! Voici le <em>topo:</em> il faut rétablir le bon usage de la typographie; c’est important.</p>'
+      string: '<p>Bonjour <b>typographer</b>! Voici le <em>topo:</em> il faut rétablir le «bon usage» de la typographie; c’est important.</p>'
     })
-  ).toBe('<p>Bonjour <b>typographer</b>\u202f! Voici le <em>topo\u00a0:</em> il faut rétablir le bon usage de la typographie\u202f; c’est important.</p>');
+  ).toBe('<p>Bonjour <b>typographer</b>\u202f! Voici le <em>topo\u00a0:</em> il faut rétablir le «\u202fbon usage\u202f» de la typographie\u202f; c’est important.</p>');
 });
 
 test('[html] Can format html strings', () => {
@@ -15,7 +15,7 @@ test('[html] Can format html strings', () => {
     typographer({
       locale: 'fr_FR',
       output_format: 'html',
-      string: '<p>Bonjour <b>typographer</b>! Voici le <em>topo:</em> il faut rétablir le bon usage de la typographie; c’est important.</p>'
+      string: '<p>Bonjour <b>typographer</b>! Voici le <em>topo:</em> il faut rétablir le &laquo;bon usage&#x00BB; de la typographie; c’est important.</p>'
     })
-  ).toBe('<p>Bonjour <b>typographer</b>&#8239;! Voici le <em>topo&nbsp;:</em> il faut rétablir le bon usage de la typographie&#8239;; c’est important.</p>');
+  ).toBe('<p>Bonjour <b>typographer</b>&#8239;! Voici le <em>topo&nbsp;:</em> il faut rétablir le &laquo;&#8239;bon usage&#8239;&#x00BB; de la typographie&#8239;; c’est important.</p>');
 });
