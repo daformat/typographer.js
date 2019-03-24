94b822e8b4c57a51562d18493a49cc647fdb1b0c
daformat <mat.jouhet@gmail.com>
2019-03-24 11:22:11 +0100

    Use RegExp constructor for patterns instead of literal regexes
    So we can use the super long `space` non-capturing group as a variable, we don't want to duplicate it everywhere and make things illegible

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@
 // otherwise it's going to be pretty verbose and difficult to
 // work with the expressions
 
+// Full (hopefully) regex for both textual and html entities spaces:
+const space = '(?:\\s|&#x20;|&#32;|&thinsp;|&#x2009;|&#8201;|&#x200a;|&#8202;|&#x205f;|&#8287;|&#x1680;|&#5760;|&#x2000;|&#8192;|&#x2001;|&#8193;|&ensp;|&#x2002;|&#8194;|&emsp;|&#x2003;|&#8195;|&#x2004;|&#8196;|&#x2005;|&#8197;|&#x2006;|&#8198;|&nbsp;|&#xa0;|&#160;|&#x202f;|&#8239;|&#x2007;|&#8199;|&#x2008;|&#8200;|&#x3000;|&#12288;|&#xfeff;|&#65279;|&Tab;|&#x9;|&#9;|&#xb;|&#11;)';
+
 const rules = [
   {
     // replace space(s) before periods, except within ellipsis
@@ -11,7 +14,10 @@ const rules = [
       'fr', 'fr_FR', 'fr_BE', 'fr_CA', 'fr_CH',
       'en', 'en_US', 'en_GB', 'en_CA', 'en_AU', 'en_NZ', 'en_IN'
     ],
-    pattern: /(?<!(\s\.){1,2})\s*\.(?!(\s\.){1,2})/g,
+    pattern: new RegExp(
+      `(?<!(${space}\\.){1,2})${space}*\\.(?!(${space}\\.){1,2})`,
+      'g'
+    ),
     replacement: {
       text: '.',
       html: '.'
@@ -23,7 +29,10 @@ const rules = [
       'fr', 'fr_FR', 'fr_BE', 'fr_CA', 'fr_CH',
       'en', 'en_US', 'en_GB', 'en_CA', 'en_AU', 'en_NZ', 'en_IN'
     ],
-    pattern: /\. \. \./g,
+    pattern: new RegExp(
+      `\\.${space}\\.${space}\\.`,
+      'g'
+    ),
     replacement: {
       text: '.\u00a0.\u00a0.',
       html: '.&nbsp;.&nbsp;.'
@@ -35,7 +44,10 @@ const rules = [
       'fr', 'fr_FR', 'fr_BE', 'fr_CA', 'fr_CH',
       'en', 'en_US', 'en_GB', 'en_CA', 'en_AU', 'en_NZ', 'en_IN'
     ],
-    pattern: /\s+(\.(\s|&nbsp;)?\.(\s|&nbsp;)?\.)/g,
+    pattern: new RegExp(
+      `${space}+(\\.(${space}|&nbsp;)?\\.(${space}|&nbsp;)?\\.)`,
+      'g'
+    ),
     replacement: {
       text: ' $1',
       html: ' $1'
@@ -47,7 +59,10 @@ const rules = [
       'fr', 'fr_FR', 'fr_BE', 'fr_CA', 'fr_CH',
       'en', 'en_US', 'en_GB', 'en_CA', 'en_AU', 'en_NZ', 'en_IN'
     ],
-    pattern: /\s+,/g,
+    pattern: new RegExp(
+      `${space}+,`,
+      'g'
+    ),
     replacement: {
       text: ',',
       html: ','
@@ -57,7 +72,10 @@ const rules = [
     // replace any amount of space(s) before a colon
     // with a non-breaking space
     locales: ['fr', 'fr_FR'],
-    pattern: /\s*:/g,
+    pattern: new RegExp(
+      `${space}*:`,
+      'g'
+    ),
     replacement: {
       text: '\u00a0:',
       html: '&nbsp;:'
@@ -68,7 +86,10 @@ const rules = [
     // with a narrow non-breaking space
     // We need to make sure we don't match the `;` on an html entity
     locales: ['fr', 'fr_FR'],
-    pattern: /\s*(?<!&#?\w+);/g,
+    pattern: new RegExp(
+      `${space}*(?<!&#?\\w+);`,
+      'g'
+    ),
     replacement: {
       text: '\u202f;',
       html: '&#8239;;'
@@ -78,7 +99,10 @@ const rules = [
     // replace any amount of space(s) before an ? or an !
     // with a narrow non-breaking space
     locales: ['fr', 'fr_FR'],
-    pattern: /\s*([?!])/g,
+    pattern: new RegExp(
+      `${space}*([?!])`,
+      'g'
+    ),
     replacement: {
       text: '\u202f$1',
       html: '&#8239;$1'
@@ -91,7 +115,10 @@ const rules = [
       'fr', 'fr_FR', 'fr_BE', 'fr_CA', 'fr_CH',
       'en', 'en_US', 'en_GB', 'en_CA', 'en_AU', 'en_NZ', 'en_IN'
     ],
-    pattern: /(«|&laquo;|&#x00AB;)\s*(.*?)\s*(»|&raquo;|&#x00BB;)/g,
+    pattern: new RegExp(
+      `(«|&laquo;|&#x00AB;)${space}*(.*?)${space}*(»|&raquo;|&#x00BB;)`,
+      'g'
+    ),
     replacement: {
       text: '$1\u202f$2\u202f$3',
       html: '$1&#8239;$2&#8239;$3'
@@ -104,7 +131,10 @@ const rules = [
       'fr', 'fr_FR', 'fr_BE', 'fr_CA', 'fr_CH',
       'en', 'en_US', 'en_GB', 'en_CA', 'en_AU', 'en_NZ', 'en_IN'
     ],
-    pattern: /(‹|&lsaquo;|&#x2039;)\s*(.*?)\s*(›|&rsaquo;|&#x203A;)/g,
+    pattern: new RegExp(
+      `(‹|&lsaquo;|&#x2039;)${space}*(.*?)${space}*(›|&rsaquo;|&#x203A;)`,
+      'g'
+    ),
     replacement: {
       text: '$1\u202f$2\u202f$3',
       html: '$1&#8239;$2&#8239;$3'
@@ -116,7 +146,10 @@ const rules = [
       'fr', 'fr_FR', 'fr_BE', 'fr_CA', 'fr_CH',
       'en', 'en_US', 'en_GB', 'en_CA', 'en_AU', 'en_NZ', 'en_IN'
     ],
-    pattern: /(“|&ldquo;|&#x201C;)\s*(.*?)\s*(”|&rdquo;|&#x201D;)/g,
+    pattern: new RegExp(
+      `(“|&ldquo;|&#x201C;)${space}*(.*?)${space}*(”|&rdquo;|&#x201D;)`,
+      'g'
+    ),
     replacement: {
       text: '$1$2$3',
       html: '$1$2$3'
@@ -128,7 +161,10 @@ const rules = [
       'fr', 'fr_FR', 'fr_BE', 'fr_CA', 'fr_CH',
       'en', 'en_US', 'en_GB', 'en_CA', 'en_AU', 'en_NZ', 'en_IN'
     ],
-    pattern: /(‘|&lsquo;|&#x2018;)(?:\s|&nbsp;|&thinsp;|&#8239;|&#x202f;)*(.*?)(?:\s|&nbsp;|&thinsp;|&#8239;|&#x202f;)*(’|&rsquo;|&#x2019;)/g,
+    pattern: new RegExp(
+      `(‘|&lsquo;|&#x2018;)${space}*(.*?)${space}*(’|&rsquo;|&#x2019;)`,
+      'g'
+    ),
     replacement: {
       text: '$1$2$3',
       html: '$1$2$3'
@@ -140,7 +176,9 @@ const rules = [
       'fr', 'fr_FR', 'fr_BE', 'fr_CA', 'fr_CH',
       'en', 'en_US', 'en_GB', 'en_CA', 'en_AU', 'en_NZ', 'en_IN'
     ],
-    pattern: /(\()(?:\s|&nbsp;|&thinsp;|&#8239;|&#x202f;)*(.*?)(?:\s|&nbsp;|&thinsp;|&#8239;|&#x202f;)*(\))/g,
+    pattern: new RegExp(
+      `(\\()${space}*(.*?)${space}*(\\))`,
+      'g',
     replacement: {
       text: '$1$2$3',
       html: '$1$2$3'
@@ -152,7 +190,10 @@ const rules = [
       'fr', 'fr_FR', 'fr_BE', 'fr_CA', 'fr_CH',
       'en', 'en_US', 'en_GB', 'en_CA', 'en_AU', 'en_NZ', 'en_IN'
     ],
-    pattern: /(\[)(?:\s|&nbsp;|&thinsp;|&#8239;|&#x202f;)*(.*?)(?:\s|&nbsp;|&thinsp;|&#8239;|&#x202f;)*(\])/g,
+    pattern: new RegExp(
+      `(\\[)${space}*(.*?)${space}*(\\])`;
+      'g'
+    ),
     replacement: {
       text: '$1$2$3',
       html: '$1$2$3'
