00f2692634c984d32bc1dd240daa66dc429b5e65
daformat <mat.jouhet@gmail.com>
2019-03-24 18:10:08 +0100

    Add test for space html entities

--- test/fr_FR-exclamation-mark.test.js
+++ test/fr_FR-exclamation-mark.test.js
@@ -50,6 +50,16 @@ test('[text] Replace any type of spaces before `!` with a single narrow non-brea
   ).toBe('Bonjour typographer\u202f!');
 });
 
+test('[text] Replace any space html entities before `!` with a single narrow non-breaking space', () => {
+  expect(
+    typographer({
+      locale: 'fr_FR',
+      output_format: 'text',
+      string: 'Bonjour typographer &#x20;&#32;&thinsp;&#x2009;&#8201;&#x200a;&#8202;&#x205f;&#8287;&#x1680;&#5760;&#x2000;&#8192;&#x2001;&#8193;&ensp;&#x2002;&#8194;&emsp;&#x2003;&#8195;&#x2004;&#8196;&#x2005;&#8197;&#x2006;&#8198;&nbsp;&#xa0;&#160;&#x202f;&#8239;&#x2007;&#8199;&#x2008;&#8200;&#x3000;&#12288;&#xfeff;&#65279;&Tab;&#x9;&#9;&#xb;&#11;!'
+    })
+  ).toBe('Bonjour typographer\u202f!');
+});
+
 test('[html] Replace any type of spaces before `!` with a single narrow non-breaking space', () => {
   expect(
     typographer({
