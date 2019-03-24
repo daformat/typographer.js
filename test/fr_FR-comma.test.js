02fe7f22dd2862eaca32d2fd2ed210eccd4fdc1a
daformat <mat.jouhet@gmail.com>
2019-03-24 17:28:40 +0100

    Add test for html entities

--- test/fr_FR-comma.test.js
+++ test/fr_FR-comma.test.js
@@ -29,3 +29,13 @@ test('[text] Remove any type of space before `,`', () => {
     })
   ).toBe('Bonjour typographer, enchanté de faire votre connaissance.');
 });
+
+test('[text] Remove any space html entity before `,`', () => {
+  expect(
+    typographer({
+      locale: 'fr_FR',
+      output_format: 'text',
+      string: 'Bonjour typographer &#x20;&#32;&thinsp;&#x2009;&#8201;&#x200a;&#8202;&#x205f;&#8287;&#x1680;&#5760;&#x2000;&#8192;&#x2001;&#8193;&ensp;&#x2002;&#8194;&emsp;&#x2003;&#8195;&#x2004;&#8196;&#x2005;&#8197;&#x2006;&#8198;&nbsp;&#xa0;&#160;&#x202f;&#8239;&#x2007;&#8199;&#x2008;&#8200;&#x3000;&#12288;&#xfeff;&#65279;&Tab;&#x9;&#9;&#xb;&#11; , enchanté de faire votre connaissance.'
+    })
+  ).toBe('Bonjour typographer, enchanté de faire votre connaissance.');
+});
