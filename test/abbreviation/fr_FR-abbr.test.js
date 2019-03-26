44d084d7856151a6cde6cb0922bfd85a15bc6e8f
daformat <mat.jouhet@gmail.com>
2019-03-26 17:18:23 +0100

    Add more tests for etc.

--- test/abbreviation/fr_FR-abbr.test.js
+++ test/abbreviation/fr_FR-abbr.test.js
@@ -9,5 +9,29 @@ test('[text] Disallow ellipsis after `etc`', () => {
         output_format: 'text'
       }
     )
-  ).toBe('Pèche, pomme, poire etc.');
+  ).toBe('Pèche, pomme, poire\u00a0etc.');
+});
+
+test('[text] Disallow ellipsis after `etc` (2)', () => {
+  expect(
+    typographer(
+      'Pèche, pomme, poire etc..',
+      {
+        locale: 'fr_FR',
+        output_format: 'text'
+      }
+    )
+  ).toBe('Pèche, pomme, poire\u00a0etc.');
+});
+
+test('[text] Disallow ellipsis after `etc` (3)', () => {
+  expect(
+    typographer(
+      'Pèche, pomme, poire etc......',
+      {
+        locale: 'fr_FR',
+        output_format: 'text'
+      }
+    )
+  ).toBe('Pèche, pomme, poire\u00a0etc.');
 });
