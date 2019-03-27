6c756ea6970e4dc9176af70cc3847d1ea22990ee
daformat <mat.jouhet@gmail.com>
2019-03-27 12:34:36 +0100

    Add test with html comment

--- test/punctuation/fr_FR-html.test.js
+++ test/punctuation/fr_FR-html.test.js
@@ -23,3 +23,16 @@ test('[html] Can format html strings', () => {
     )
   ).toBe('<p>Bonjour <b>typographer</b>&#8239;! Voici le <em>topo&nbsp;:</em> il faut rétablir le &laquo;&#8239;bon usage&#8239;&#x00BB; de la typographie&#8239;; c’est important.</p>');
 });
+
+
+test('[html] Can format html strings with html comments', () => {
+  expect(
+    typographer(
+      '<!-- Ceci est un commentaire --><p>Bonjour <b>typographer</b>! Voici le <em>topo:</em> il faut rétablir le &laquo;bon usage&#x00BB; de la typographie; c’est important.</p>',
+      {
+        locale: 'fr_FR',
+        output_format: 'html',
+      }
+    )
+  ).toBe('<!-- Ceci est un commentaire --><p>Bonjour <b>typographer</b>&#8239;! Voici le <em>topo&nbsp;:</em> il faut rétablir le &laquo;&#8239;bon usage&#8239;&#x00BB; de la typographie&#8239;; c’est important.</p>');
+});
