251cb93afed3f7a868a2a17d01c95fb42fc93666
daformat <mat.jouhet@gmail.com>
2019-03-27 20:12:31 +0100

    Allow input to be an Element
    If it's the case, we use a TreeWalker to process its chidlren. The element and its children are processed only **if they are not themselves or do not contain certain types of elements** (like code, pre, etc.)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,53 @@ import composition from './rules/composition';
 // Concatenate rules
 const rules = [...punctuation, ...abbreviation, ...composition];
 
+const isElement = o => {
+  return (
+    typeof HTMLElement === 'object' ?
+      o instanceof HTMLElement :
+      o && typeof o === 'object' && o !== null && o.nodeType === 1 && typeof o.nodeName==='string'
+  );
+};
+
+const isDisallowed = node => {
+  return (
+    node.nodeName === 'PRE' ||
+    node.nodeName === 'CODE' ||
+    node.nodeName === 'KBD' ||
+    node.nodeName === 'SAMP' ||
+    node.nodeName === 'VAR' ||
+    node.nodeName === 'SCRIPT' ||
+    node.nodeName === 'STYLE' ||
+    node.nodeName === 'IFRAME'
+  );
+};
+
+const filter = node => {
+  if (isDisallowed(node)) {
+    return NodeFilter.FILTER_SKIP;
+  } else {
+    var t = [].find.call(node.children, child => {
+      return isDisallowed(child);
+    });
+    if (t) {
+      return NodeFilter.FILTER_SKIP;
+    } else {
+      return NodeFilter.FILTER_ACCEPT;
+    }
+  }
+};
+
+const apply_rules = (string, applicable_rules, output_format) => {
+  // Apply each rule one after the other
+  for (const rule of applicable_rules) {
+    string = string.replace(
+      rule.pattern,
+      rule.replacement[output_format]
+    );
+  }
+  return string;
+};
+
 const typographer = (
   input = '',
   {
@@ -36,21 +83,37 @@ const typographer = (
   }
 
   // Test input type
-  // For the moment we only accept strings
-  let string = '';
   if (typeof input === 'string') {
-    string = input;
-  }
-
-  // Apply each rule one after the other
-  for (const rule of applicable_rules) {
-    string = string.replace(
-      rule.pattern,
-      rule.replacement[output_format]
+    // We're dealing with a string, proceed with applying applicable_rules
+    return apply_rules(input, applicable_rules, output_format);
+  } else if (isElement(input)) {
+    // We're dealing with a DOM element, we'll use a TreeWalker
+    const treeWalker = document.createTreeWalker(
+      input,
+      NodeFilter.SHOW_ELEMENT,
+      { acceptNode: filter },
+      false
     );
+
+    let nodeList = [];
+
+    while(treeWalker.nextNode()) {
+      // Since we're working with innerHTML we only want to work on top-level
+      // parent nodes, and avoid processing the same thing more than once
+      if (
+        nodeList.length === 0 ||
+        !nodeList[nodeList.length - 1].contains(treeWalker.currentNode)
+      ) {
+        nodeList.push(treeWalker.currentNode);
+        treeWalker.currentNode.innerHTML = process(
+          treeWalker.currentNode.innerHTML,
+          applicable_rules,
+          output_format
+        );
+      }
+    }
   }
 
-  return string;
 };
 
 export default typographer;
