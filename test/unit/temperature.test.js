879f1bac6b84e44e09fc7c994234c9422fedf2ed
daformat <mat.jouhet@gmail.com>
2019-03-28 17:15:09 +0100

    Add test for Fahrenheit temperatures

--- test/unit/temperature.test.js
+++ test/unit/temperature.test.js
@@ -25,3 +25,30 @@ test('[text] Non-breaking space before °C', () => {
     )
   ).toBe('La température normale du corps humain se situe entre 36,1&nbsp;°C et 37,8&nbsp;°C');
 });
+
+
+test('[text] Non-breaking space before °F', () => {
+  expect(
+    typographer(
+      'Il fait au moins -8000°F',
+      {
+        locale: 'fr_FR',
+        output_format: 'html',
+        disable_rules: ['composition']
+      }
+    )
+  ).toBe('Il fait au moins -8000&nbsp;°F');
+});
+
+test('[text] Non-breaking space before °F', () => {
+  expect(
+    typographer(
+      'La température normale du corps humain se situe entre 96,98 °F et 100,04   °F',
+      {
+        locale: 'fr_FR',
+        output_format: 'html',
+        disable_rules: ['composition']
+      }
+    )
+  ).toBe('La température normale du corps humain se situe entre 96,98&nbsp;°F et 100,04&nbsp;°F');
+});
