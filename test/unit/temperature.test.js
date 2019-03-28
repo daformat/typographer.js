13bc750f4753475ec666893a50aff8908206b963
daformat <mat.jouhet@gmail.com>
2019-03-28 17:33:22 +0100

    Add test for unknown degree units

--- test/unit/temperature.test.js
+++ test/unit/temperature.test.js
@@ -1,6 +1,6 @@
 import typographer from '../../dist/typographer.umd';
 
-test('[text] Non-breaking space before °C', () => {
+test('[html] Non-breaking space before °C', () => {
   expect(
     typographer(
       'Il fait au moins -8000°C',
@@ -13,7 +13,7 @@ test('[text] Non-breaking space before °C', () => {
   ).toBe('Il fait au moins -8000&nbsp;°C');
 });
 
-test('[text] Non-breaking space before °C', () => {
+test('[html] Non-breaking space before °C', () => {
   expect(
     typographer(
       'La température normale du corps humain se situe entre 36,1 °C et 37,8   °C',
@@ -27,7 +27,7 @@ test('[text] Non-breaking space before °C', () => {
 });
 
 
-test('[text] Non-breaking space before °F', () => {
+test('[html] Non-breaking space before °F', () => {
   expect(
     typographer(
       'Il fait au moins -8000°F',
@@ -40,7 +40,7 @@ test('[text] Non-breaking space before °F', () => {
   ).toBe('Il fait au moins -8000&nbsp;°F');
 });
 
-test('[text] Non-breaking space before °F', () => {
+test('[html] Non-breaking space before °F', () => {
   expect(
     typographer(
       'La température normale du corps humain se situe entre 96,98 °F et 100,04   °F',
@@ -51,4 +51,17 @@ test('[text] Non-breaking space before °F', () => {
       }
     )
   ).toBe('La température normale du corps humain se situe entre 96,98&nbsp;°F et 100,04&nbsp;°F');
+});
+
+test('[html] Doesn’t add non-breaking on unknown units', () => {
+  expect(
+    typographer(
+      'Une valeur comprise en -42°CA et 42°FK',
+      {
+        locale: 'fr_FR',
+        output_format: 'html',
+        disable_rules: ['composition']
+      }
+    )
+  ).toBe('Une valeur comprise en -42°CA et 42°FK');
 });
