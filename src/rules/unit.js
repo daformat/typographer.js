25c58182600939c74f8745928081931b45e74f3a
daformat <mat.jouhet@gmail.com>
2019-03-28 20:21:45 +0100

    Add test and rules for metric distance units

--- src/rules/unit.js
+++ src/rules/unit.js
@@ -1,6 +1,7 @@
 import {space} from '../helpers/regex';
 
 const number = '(?:\\d+(?:[.,]\\d+)?)';
+const prefix = '(?:[TGMkhdcmnpfa]|da|µ|&mu;|&#x3bc;|&#956;)';
 
 const unit = [
   {
@@ -17,6 +18,38 @@ const unit = [
     replacement: {
       text: '$1\u00a0$2',
       html: '$1&nbsp;$2'
+    }
+  },
+  {
+    // Non breaking space before distance units (metric)
+    name: 'nbsp-before-unit-distance-metric',
+    locales: [
+      'fr', 'fr_FR', 'fr_BE', 'fr_CA', 'fr_CH',
+      'en', 'en_US', 'en_GB', 'en_CA', 'en_AU', 'en_NZ', 'en_IN'
+    ],
+    pattern: new RegExp(
+      `(${number})${space}*(${prefix}?m)\\b`,
+      'g'
+    ),
+    replacement: {
+      text: '$1\u00a0$2',
+      html: '$1&nbsp;$2'
+    }
+  },
+  {
+    // Non breaking space before distance units (metric) 2
+    name: 'nbsp-before-unit-distance-metric-2',
+    locales: [
+      'fr', 'fr_FR', 'fr_BE', 'fr_CA', 'fr_CH',
+      'en', 'en_US', 'en_GB', 'en_CA', 'en_AU', 'en_NZ', 'en_IN'
+    ],
+    pattern: new RegExp(
+      `(${number})${space}*(${prefix}?m)${space}*(${number})`,
+      'g'
+    ),
+    replacement: {
+      text: '$1\u00a0$2\u00a0$3',
+      html: '$1&nbsp;$2&nbsp;$3'
     }
   }
 ];
---
+++ test/unit/distance-metric.test.js
@@ -0,0 +1,40 @@
+import typographer from '../../dist/typographer.umd';
+
+test('[html] Non-breaking space before `m`', () => {
+  expect(
+    typographer(
+      'Il mesure 2m',
+      {
+        locale: 'fr_FR',
+        output_format: 'html',
+        disable_rules: ['composition']
+      }
+    )
+  ).toBe('Il mesure 2&nbsp;m');
+});
+
+test('[html] Non-breaking space before `m`', () => {
+  expect(
+    typographer(
+      'Il mesure 1m80',
+      {
+        locale: 'fr_FR',
+        output_format: 'html',
+        disable_rules: ['composition']
+      }
+    )
+  ).toBe('Il mesure 1&nbsp;m&nbsp;80');
+});
+
+test('[html] Non-breaking space before `km`', () => {
+  expect(
+    typographer(
+      'Continuer sur 2km',
+      {
+        locale: 'fr_FR',
+        output_format: 'html',
+        disable_rules: ['composition']
+      }
+    )
+  ).toBe('Continuer sur 2&nbsp;km');
+});
