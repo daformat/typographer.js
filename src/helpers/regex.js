f6aaf91a75fb9115f340601533d75b35b18093e2
daformat <mat.jouhet@gmail.com>
2019-04-01 15:50:53 +0200

    compose tag and opening_tag regex

--- src/helpers/regex.js
+++ src/helpers/regex.js
@@ -2,9 +2,12 @@
 export const space = '(?:\\s|&#x20;|&#32;|&thinsp;|&#x2009;|&#8201;|&#x200a;|&#8202;|&#x205f;|&#8287;|&#x1680;|&#5760;|&#x2000;|&#8192;|&#x2001;|&#8193;|&ensp;|&#x2002;|&#8194;|&emsp;|&#x2003;|&#8195;|&#x2004;|&#8196;|&#x2005;|&#8197;|&#x2006;|&#8198;|&nbsp;|&#xa0;|&#160;|&#x202f;|&#8239;|&#x2007;|&#8199;|&#x2008;|&#8200;|&#x3000;|&#12288;|&#xfeff;|&#65279;|&Tab;|&#x9;|&#9;|&#xb;|&#11;)';
 
 // HE'S COMING - https://stackoverflow.com/a/1732454 - regex adapted from https://haacked.com/archive/2004/10/25/usingregularexpressionstomatchhtml.aspx/
-export const tag = '</?\\w+(?:(?:\\s+\\w+(?:-\\w+)*(?:\\s*=\\s*(?:".*?"|\'.*?\'|[^\'">\\s]+))?)+\\s*|\\s*)/?>';
+const tag_attribute = `\\w+(?:-\\w+)*(?:\\s*=\\s*(?:".*?"|'.*?'|[^'">\\s]+))?`;
+export const tag = `</?\\w+(?:(?:\\s+${tag_attribute})+\\s*|\\s*)/?>`;
 
-export const opening_tag = tag_name => (`<${tag_name}(?:(?:\\s+\\w+(?:-\\w+)*(?:\\s*=\\s*(?:".*?"|'.*?'|[^'">\\s]+))?)+\\s*|\\s*)/?>`);
+export const opening_tag = tag_name => (
+  `<${tag_name}(?:(?:\\s+${tag_attribute})+\\s*|\\s*)/?>`
+);
 
 // This is far from being perfect but it does prevent transforming things within tag attributes values. Unfortunately it also prevents transforming things like the colon in: `<p>Hello color: red" attr ></p>` because the right part up to the closing </p> looks like what would be within a html tag - But it's better not to transform those than to transform what could be part of an html tag and mess up inline styles, scripts, or whatever.
 export const not_in_tag_attribute_value = what => (`(?!<\\w+(?:(?:\\s+\\w+(?:-\\w+)*(?:\\s*=\\s*(?:".*?"|'.*?'|[^'">\\s]+))?)+\\s*|\\s*))${what}(?!.*?["'](?:(?:\\s+\\w+(?:-\\w+)*(?:\\s*=\\s*(?:".*?"|'.*?'|[^'">\\s]+))?)+\\s*|\\s*)/?>)`);
